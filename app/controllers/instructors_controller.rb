e44b2c3717b7ca31b80433568c9c90252810e41d
Nicholas Galante <nicholasgalante94@gmail.com>
2023-06-23 18:24:03 -0400

    Create CRUD route and actions

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,2 +1,36 @@
 class InstructorsController < ApplicationController
+   def index
+      instructors = Instructor.all 
+      render json: instructors, status: :ok
+   end
+
+   def show
+      instructor = Instructor.find(params[:id])
+      render json: instructor, status: :ok
+   end
+
+   def update
+      instructor = Instructor.find(params[:id])
+      if instructor
+         instructor.update(instructor_params)
+      else 
+         render json: { errror: "Instructor not found"}, status: :not_found
+      end
+   end
+
+   def destroy
+      instructor = Instructor.find(params[:id])
+      if instructor
+      instructor.destroy
+      render json: {}
+      else 
+         render json: { errror: "Instructor not found"}, status: :not_found
+      end
+   end
+
+   private
+
+   def instructor_params
+      params.permit(:name)
+   end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,36 @@
 class StudentsController < ApplicationController
+   def index
+      students = Student.all 
+      render json: students, status: :ok
+   end
+
+   def show
+      student = Student.find(params[:id])
+      render json: student, status: :ok
+   end
+
+   def update
+      student = Student.find(params[:id])
+      if student
+         student.update(student_params)
+      else 
+         render json: { errror: "Student not found"}, status: :not_found
+      end
+   end
+
+   def destroy
+      student = Student.find(params[:id])
+      if student
+      student.destroy
+      render json: {}
+      else 
+         render json: { errror: "Student not found"}, status: :not_found
+      end
+   end
+
+   private
+
+   def student_params
+      params.permit(:name, :major, :age)
+   end
 end
